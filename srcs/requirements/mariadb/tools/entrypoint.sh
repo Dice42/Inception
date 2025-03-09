3a3927179d23175e925ae3be18259fbb701ebacc
dice42 <mohammoh@student.42abudhabi.ae>
2025-03-09 06:56:39 -0400

    nginx hosting

---
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+/etc/init.d/mariadb setup
+
+rc-service mariadb start
+
+echo "CREATE DATABASE IF NOT EXISTS $MARIADB_NAME;" > md.file
+echo "CREATE USER IF NOT EXISTS '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_PWD' ;" >> md.file
+echo "GRANT ALL PRIVILEGES ON $MARIADB_NAME.* TO '$MARIADB_USER'@'%' ;" >> md.file
+echo "FLUSH PRIVILEGES;" >> md.file
+echo "ALTER USER '$MARIADB_ROOT_USER'@'localhost' IDENTIFIED BY '$MARIADB_ROOT_PASS';" >> md.file
+
+mariadb < md.file
+
+
+sed -i 's/skip-networking/#skip-networking/g' /etc/my.cnf.d/mariadb-server.cnf
+sed -i 's/#bind-address=0.0.0.0/bind-address=0.0.0.0/g' /etc/my.cnf.d/mariadb-server.cnf
+
+rc-service mariadb restart
+rc-service mariadb stop
+
+#manually start the mariadb deamon --base directory where mariadb installed --data directory where mariadb stores all its tables logs schemas indexes --plugin dir where its stores its engine plugin authintication --pid 
+/usr/bin/mariadbd --basedir=/usr --datadir=/var/lib/mysql --plugin-dir=/usr/lib/mariadb/plugin --user=mysql --pid-file=/run/mysqld/mariadb.pid
